89d7e85667212293c41aa89e46f7ffe666742356
Kamenev Nikolay <79969997909@mail.ru>
2021-04-22 19:22:50 +0300

    create server

---
+++ controllers/cards.js
@@ -0,0 +1,80 @@
+const Card = require('../models/card');
+
+// Получить все карточки
+const getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.status(200).send(cards))
+    .catch((err) => res.status(500).send(err));
+};
+
+// Создать карточку
+const createCard = (req, res) => {
+  const {name, link} = req.body;
+  const owner = req.user._id;
+  Card.create({name, link, owner})
+    .then((card) => res.status(200).send(card))
+    .catch((err) => res.status(500).send(err));
+};
+
+// Удалить карточку
+const deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({message: 'Нет карточки с таким id'});
+      }
+      return res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({message: 'Id неверный'});
+      }
+      return res.status(500).send(err);
+    });
+};
+
+// Поставить лайк
+const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId,
+    {$addToSet: {likes: req.user._id}},
+    {new: true, runValidators: true})
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({message: 'Нет карточки с таким id'});
+      }
+      return res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({message: 'Id неверный'});
+      }
+      return res.status(500).send(err);
+    });
+};
+
+// Убрать лайк
+const dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId,
+    {$pull: {likes: req.user._id}},
+    {new: true, runValidators: true})
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({message: 'Нет карточки с таким id'});
+      }
+      return res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({message: 'Id неверный'});
+      }
+      return res.status(500).send(err);
+    });
+};
+
+module.exports = {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+}
