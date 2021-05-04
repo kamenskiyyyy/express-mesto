7058628b4b39ab871a4f735235f21f11b7841ae3
Kamenev Nikolay <79969997909@mail.ru>
2021-05-04 14:20:23 +0300

    feat 14 project, auth and register

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const AuthError = require('../errors/AuthError');
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer')) {
+    throw new AuthError('Необходима авторизация');
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    // верифицируем токен
+    payload = jwt.verify(token, '41452244c0ff5e928b37b9ced5a7670f52fe8b5a7aa431eb88a0ed06ad321295');
+  } catch (err) {
+    throw new AuthError('Необходима авторизация');
+  }
+
+  req.user = payload;
+  next();
+};
+
+module.exports = auth;
