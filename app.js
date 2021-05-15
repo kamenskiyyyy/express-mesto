68848470fecb09b720ff2eaa07fb314aa91c9fa5
Kamenev Nikolay <79969997909@mail.ru>
2021-05-15 13:57:39 +0300

    add CORS

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
-const { createUser, login } = require('./controllers/users');
+const {
+  createUser,
+  login
+} = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const cors = require('cors');
+const {
+  requestLogger,
+  errorLogger
+} = require('./middlewares/logger');
 
 dotenv.config();
 const { PORT = 3000 } = process.env;
+
+const corsOptions = {
+  origin: 'mesto.kamenskiyyyy.nomoredomains.club',
+};
 
 const app = express();
 
@@ -19,8 +31,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
 
 app.post('/signin', login);
 app.post('/signup', createUser);
@@ -30,15 +46,22 @@ app.use(auth);
 app.use('/', userRouter);
 app.use('/', cardRouter);
 
+app.use(errorLogger);
+
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'Произошла ошибка на сервере' : message,
-  });
+  const {
+    statusCode = 500,
+    message
+  } = err;
+  res.status(statusCode)
+    .send({
+      message: statusCode === 500 ? 'Произошла ошибка на сервере' : message,
+    });
 });
 
 app.get('/*', (req, res) => {
-  res.status(404).send({ message: 'ресурс не найден' });
+  res.status(404)
+    .send({ message: 'ресурс не найден' });
 });
 
 app.listen(PORT);
