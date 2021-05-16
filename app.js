e4038d69295e7f5b5157f3321f3c0c5e0e36f071
Kamenev Nikolay <79969997909@mail.ru>
2021-05-16 19:47:25 +0300

    code review

--- app.js
+++ app.js
@@ -57,14 +57,12 @@ app.get('/crash-test', () => {
 app.post('/signin', validateSignIn, login); // вторым аргументом передаем middleware для валидации приходящих данных до обращения к бд
 app.post('/signup', validateSignUp, createUser);
 
-app.use(auth);
-
-app.use('/', userRouter);
-app.use('/', cardRouter);
-
 app.use('/*', () => {
   throw new NotFoundError('Такой страницы не существует');
 });
+
+app.use('/', auth, userRouter);
+app.use('/', auth, cardRouter);
 
 app.use(errorLogger);
 app.use(errors()); // обработчик ошибок celebrate
